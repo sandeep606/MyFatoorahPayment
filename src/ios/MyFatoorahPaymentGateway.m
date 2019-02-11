e9a27ace71db114b25b3c0a7cfe7c7083e14b594
sandeep <sandeep.rawat@evontech.com>
2019-02-11 14:10:16 +0530

    Fatoorah framework added

--- src/ios/MyFatoorahPaymentGateway.m
+++ src/ios/MyFatoorahPaymentGateway.m
@@ -1,6 +1,7 @@
 /********* MyFatoorahPaymentGateway.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <MFSDK/MFSDK.h>
 
 @interface MyFatoorahPaymentGateway : CDVPlugin {
   // Member variables go here.
@@ -13,6 +14,22 @@
 
 - (void)presentPaymentModule:(CDVInvokedUrlCommand*)command
 {
+
+    [MFSettings.sharedInstance setMerchantWithMerchantCodeWithMerchantCode:@"999999"
+                                                              merchantName:@"Web Pay"
+                                                          merchantUserName:@"testapi@myfatoorah.com"
+                                                          merchantPassword:@"E55D0"
+                                                       merchantReferenceID:@"201454542102"
+                                                         merchantReturnURL:@"https://www.google.co.in"
+                                                          merchantErrorUrl:@"https://www.yahoo.com"
+                                                                      udf1:@""
+                                                                      udf2:@""
+                                                                      udf3:@""
+                                                                      udf4:@""
+                                                                      udf5:@""
+                                                                isTestMode:YES];
+
+                                                                
     CDVPluginResult* pluginResult = nil;
     // NSString* echo = [command.arguments objectAtIndex:0];
 
